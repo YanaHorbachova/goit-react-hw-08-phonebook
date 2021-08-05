8ab736146227bb54c46149bac9a63670009533ed
YanaHorbachova <maya69877@gmail.com>
2021-08-05 12:43:27 +0300

    Updates

---
+++ static/js/home-page.1cbf03f7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[0],{115:function(o,e,t){"use strict";t.r(e);var n=t(18),c=t(1),p={textAlign:"center",padding:"20px",marginTop:"40vh",backgroundCOlor:"#ffffff"};e.default=function(){return Object(c.jsx)("h1",{style:Object(n.a)({},p),children:"Welcome to Phonebook App"})}}}]);
+//# sourceMappingURL=home-page.1cbf03f7.chunk.js.map
